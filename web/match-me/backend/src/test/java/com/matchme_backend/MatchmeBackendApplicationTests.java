ff746767fb0729ea023e48e6ed9560a5e33b8294
Artur Hartwich <artur.hartwich@gmail.comm>
2026-05-21 15:09:03 +0300

    pushing blank spring

---
+++ web/match-me/backend/src/test/java/com/matchme_backend/MatchmeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matchme_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchmeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
